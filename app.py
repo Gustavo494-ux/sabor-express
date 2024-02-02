4181df0358fa569da0f981a677b30bde92efaf41
Gustavo Gama <gustavogama494@gmail.com>
2024-02-02 11:09:33 -0300

    modulo 3

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+
+restaurantes = ['Pizza','sushi']
+
 def exibir_nome_do_programa():
     print("""
     ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -16,23 +19,58 @@ def exibir_opcoes():
     print('4. Sair restaurante\n')
 
 def finalizar_app():
-    os.system('clear')
-    print('Finalizando o app')
+    exibir_subtitulo('Finalizar app')
+
+def opcao_invalida():
+    print('Opção Inválida!\n')
+    voltar_ao_menu_principal()
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal: ')
+    main()
 
 def excolher_opcoes():
-    opcaoEscolhida = int(input('Escolha uma opção '))
+    try:
+        opcaoEscolhida = int(input('Escolha uma opção: '))
 
-    match opcaoEscolhida:
-        case 1:
-            print('Cadastrar restaurantes')
-        case 2:
-            print('Listar restaurantes')
-        case 3:
-            print('Ativar restaurantes')
-        case _:
-            finalizar_app()
+        match opcaoEscolhida:
+            case 1:
+                cadastrar_novo_restaurante()
+            case 2:
+                listar_restaurantes_cadastrados()
+            case 3:
+                print('Ativar restaurantes')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
+       
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes_cadastrados():
+    exibir_subtitulo('Listanto os restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('clear')
+    print(texto)
+    print()
 
 def main():
+    os.system('clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     excolher_opcoes()
