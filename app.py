d6129a6a0830693ecd792ba9d988d74cb904bbac
Gustavo Gama <gustavogama494@gmail.com>
2024-02-08 16:07:40 -0300

    modulo 5

--- app.py
+++ app.py
@@ -1,112 +1,13 @@
-import os
+from modelos.restaurante import Restaurante
 
-restaurantes = [
-    {'nome':'Praça','categoria':'Japonesa','ativo':False},
-    {'nome':'Pizza Suprema','categoria':'Pizza','ativo':True},
-    {'nome':'Cantina','categoria':'Italiano','ativo':False}]
+Restaurante_praca = Restaurante('praça','Gourmet')
 
-def exibir_nome_do_programa():
-    print("""
-    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-    """)
-
-def exibir_opcoes():
-    print('1. Cadastrar restaurante')
-    print('2. Listar restaurante')
-    print('3. Ativar restaurante')
-    print('4. Sair restaurante\n')
-
-def finalizar_app():
-    exibir_subtitulo('Finalizar app')
-
-def opcao_invalida():
-    print('Opção Inválida!\n')
-    voltar_ao_menu_principal()
-
-def voltar_ao_menu_principal():
-    input('\nDigite uma tecla para voltar ao menu principal: ')
-    main()
-
-def excolher_opcoes():
-    try:
-        opcaoEscolhida = int(input('Escolha uma opção: '))
-
-        match opcaoEscolhida:
-            case 1:
-                cadastrar_novo_restaurante()
-            case 2:
-                listar_restaurantes_cadastrados()
-            case 3:
-                alternar_estado_restaurante()
-            case 4:
-                finalizar_app()
-            case _:
-                opcao_invalida()
-    except:
-        opcao_invalida()
-       
-def cadastrar_novo_restaurante():
-    '''Essa função é responsável por cadastrar um novo restaurante'''
-
-    exibir_subtitulo('Cadastro de novos restaurantes')
-
-    nome_do_restaurante = input('Digite o nome do restaurante: ')
-    categoria_do_restaurante = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
-    
-    restaurantes.append({'nome':nome_do_restaurante, 'categoria':categoria_do_restaurante, 'ativo':False})
-
-    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
-    voltar_ao_menu_principal()
-
-def alternar_estado_restaurante():
-    '''Está função é responsável por inverter o status do restaurante'''
-    exibir_subtitulo('Alterando estado do restaurante')
-    nome_do_restaurante  = input('Digite o nome do restaurante que deseja: ')
-
-    restaurante_encontrado = False
-    for restaurante in restaurantes:
-        if nome_do_restaurante == restaurante['nome']:
-            restaurante_encontrado = True
-            restaurante['ativo'] = not restaurante['ativo']
-            mensagem = (f'O resturante {nome_do_restaurante} foi ativado com sucesso' 
-            if restaurante['ativo'] else f'O resturante {nome_do_restaurante} fopi desativado com sucesso')
-            print(mensagem)
-    if not restaurante_encontrado:
-        print('O restaurante não foi encontrado')
-    voltar_ao_menu_principal()
-
-
-def listar_restaurantes_cadastrados():
-    '''Está função é responsável por listar os restaurantes cadastrados'''
-    exibir_subtitulo('Listanto os restaurantes')
-
-    print(f' {"Nome do restaurante".ljust(22)} | {"Categoria".ljust(20)} | Status')
-    for restaurante in restaurantes:
-        nome = restaurante['nome']
-        categoria = restaurante['nome']
-        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
-        print(f' - {nome.ljust(20)} | {categoria.ljust(20)} | {ativo}')
-
-    voltar_ao_menu_principal()
-
-def exibir_subtitulo(texto):
-    '''Essa função é '''
-    os.system('clear')
-    linha = '*' * (len(texto))
-    print(linha)
-    print(texto)
-    print(linha)
+Restaurante_praca.receber_avaliacao('Guilherme',10)
+Restaurante_praca.receber_avaliacao('Fernanda',8)
+Restaurante_praca.receber_avaliacao('Pedro',8)
 
 def main():
-    os.system('clear')
-    exibir_nome_do_programa()
-    exibir_opcoes()
-    excolher_opcoes()
-
+   Restaurante.listar_restaurantes()
+   
 if __name__ == '__main__':
     main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,3 +1,5 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []  # Corrigido de resturantes para restaurantes
 
@@ -5,6 +7,8 @@ class Restaurante:
         self._nome = nome.title()
         self.categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
+        
         Restaurante.restaurantes.append(self)  # Corrigido de resturantes para restaurantes
 
     def __str__(self) -> str:
@@ -12,9 +16,9 @@ class Restaurante:
 
     @classmethod
     def listar_restaurantes(cls):  
-        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliação".ljust(25)} | {"Status"}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
@@ -22,10 +26,17 @@ class Restaurante:
 
     def alternar_estado(self):
             self._ativo = not self._ativo
-restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.alternar_estado()
-restaurante_pizza = Restaurante('pizza Express', 'Pizza')
 
-resturantes = [restaurante_pizza, restaurante_praca]
-
-Restaurante.listar_restaurantes()
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente,nota)
+        self._avaliacao.append(avaliacao)
+        
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        
+        media =round(soma_das_notas  / len(self._avaliacao),1)
+        return media
